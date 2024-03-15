53626a8e555acaa238745355ae4f2d2e83624b4f
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-03-15 14:23:44 -0600

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getCharacters(done){
     const results = fetch("https://rickandmortyapi.com/api/character")
     
     results
-    .then((response => response.json))
+    .then(response => response.json())
     .then(data=> {
         done(data)
     })
@@ -12,12 +12,28 @@ getCharacters(data => {
     data.results.forEach(personaje => {  
     const article = document.createRange().createContextualFragment(`
     <article>
-    <div>
-        <img src="${personaje.image}" alt="">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+            <img src="${personaje.image}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-5">
+            <div class="card-body">
+                <h5 class="card-title">${personaje.name}</h5>
+                <p>${personaje.status}</p>
+                <p>${personaje.species}</p>
+                <p>${personaje.type}</p>
+                <p>${personaje.gender}</p>
+                <p>${personaje.origin.name}</p>
+            </div>
+            </div>
+        </div>
     </div>
-        <h2>${personaje.name}</h2>
-        <span>${personaje.status}</span>
     </article>
     `)
+
+    const main = document.querySelector("#card")
+    main.append(article)
+
     })
 })
